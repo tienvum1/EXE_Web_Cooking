dbf376060c8f18da23053fa67ffc22a0e1a5b158
ablue <tienvukk1@gmail.com>
2025-05-16 22:14:35 +0700

    be
    be

--- be/server.js
+++ be/server.js
@@ -1,6 +1,7 @@
 // app.js
 const express = require('express');
 const cors = require('cors');
+const connectDB = require('./db');
 
 const app = express();
 const PORT = process.env.PORT || 3002;
@@ -14,6 +15,7 @@ app.use(cors({
   
 app.use(express.json());
 
+connectDB();
 // Route test
 app.get('/', (req, res) => {
   res.send('Healthy Cooking API is running!');
