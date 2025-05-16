53bb774ae3e90236fdd61202f3642400002dc519
ablue <tienvukk1@gmail.com>
2025-05-16 08:18:33 +0700

    Updated server
    Updated server

--- be/server.js
+++ be/server.js
@@ -3,10 +3,15 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3002;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: false
+  }));
+  
 app.use(express.json());
 
 // Route test
@@ -16,5 +21,5 @@ app.get('/', (req, res) => {
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+    console.log(`Server is running at http://localhost:${PORT}`);
+  });
