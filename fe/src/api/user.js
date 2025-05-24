f7e67c3ea65bdf42f80f87903f40b6eea1154bc4
ablue <tienvukk1@gmail.com>
2025-05-24 16:20:10 +0700

    paymen https
    paymen https

--- fe/src/api/user.js
+++ fe/src/api/user.js
@@ -1,16 +1,18 @@
 import axios from 'axios';
 
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:4567';
+
 export const getCurrentUser = async () => {
-  const res = await axios.get('http://localhost:4567/api/users/me', { withCredentials: true });
+  const res = await axios.get(`${API_URL}/api/users/me`, { withCredentials: true });
   return res.data;
 };
 
 export const fetchUserById = async (id) => {
-  const res = await axios.get(`http://localhost:4567/api/users/${id}`,{ withCredentials: true });
+  const res = await axios.get(`${API_URL}/api/users/${id}`,{ withCredentials: true });
   return res.data;
 };
 
 export const getUserWithRecipes = async (id) => {
-  const res = await axios.get(`http://localhost:4567/api/users/${id}/recipes`, { withCredentials: true });
+  const res = await axios.get(`${API_URL}/api/users/${id}/recipes`, { withCredentials: true });
   return res.data;
 };
